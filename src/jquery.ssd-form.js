20a51303f0afad4bdc84cf8873bb02c612bb574b
Sebastian <sebastian@Sebastians-Mac-Pro.local>
2016-03-14 17:53:02 +0000

    Initial plugin structure

---
+++ src/jquery.ssd-form.js
@@ -0,0 +1,30 @@
+
+/*
+ * ssdForm jQuery plugin
+ * Examples and documentation at: https://github.com/sebastiansulinski/ssd-form
+ * Copyright (c) 2016 Sebastian Sulinski
+ * Version: 1.0.0 (14-MAR-2016)
+ * Licensed under the MIT.
+ * Requires: jQuery v1.9 or later
+ */
+;(function($, undefined) {
+
+    $.fn.ssdForm = function (options) {
+
+        "use strict";
+
+        var settings = $.extend({
+
+
+
+        }, options);
+
+        return this.each(function() {
+
+            "use strict";
+
+        });
+
+    };
+
+})(window.jQuery);
